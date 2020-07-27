a9019e62bf54f9a824241965e1486ad9991e9627
Diana Odiaka-Obiagwu <59796234+dianabeth@users.noreply.github.com>
2020-07-27 23:27:16 +0100

    created routes but not working properly

--- models/celebrity.js
+++ models/celebrity.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
 
 const celebritySchema = new mongoose.Schema({
-  celebrity: {
-    name: {
-      type: String,
-      required: true
-    },
-    occupation: {
-      type: String
-    },
-    catchphrase: {
-      type: String,
-      required: true
-    }
+  name: {
+    type: String,
+    required: true
+  },
+  occupation: {
+    type: String
+  },
+  catchphrase: {
+    type: String,
+    required: true
   }
 });
 
--- routes/celebrities.js
+++ routes/celebrities.js
@@ -12,30 +12,58 @@ celebRouter.get('/', (req, res, next) => {
     });
 });
 
+celebRouter.get('/celebrities/create', (req, res, next) => {
+  res.render('celebrities/create');
+});
+
+celebRouter.post('/celebrities/create', (req, res, next) => {
+  const data = req.body;
+  Celebrity.create({
+    name: data.name,
+    occupation: data.occupation,
+    catchPhrase: data.catchPhrase
+  })
+    .then(celebrity => {
+      res.redirect('/celebrities');
+    })
+    .catch(error => {
+      console.log('error:'.error);
+      next(error);
+    });
+});
+
 celebRouter.get('/celebrities/:id', (req, res, next) => {
   const id = req.params.id;
   Celebrity.findById(id)
     .then(celebrities => {
-      res.render('celebrities/show', celebrities);
+      res.render('celebrities/show', { celebrities });
     })
     .catch(error => {
       next(error);
     });
 });
 
-celebRouter.post('/create', (req, res, next) => {
-  const data = req.body;
-  Celebrity.create(data)
-    .then(celebrity => {
-      celebrity.save();
-      res.render('celebrities/create', { celebrity });
+celebRouter.get('/celebrities/:id/edit', (req, res, next) => {
+  const id = req.params.id;
+  Celebrity.findById(id)
+    .then(celebrities => {
+      res.render('celebrities/edit', { celebrities });
     })
     .catch(error => {
-      console.log('error:'.error);
-      res.redirect('/create');
-    })
-    .then(() => {
+      next(error);
+    });
+});
+
+celebRouter.post('/celebrities/:id/edit', (req, res, next) => {
+  const id = req.params.id;
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrity.findByIdAndUpdate(id, { name, occupation, catchPhrase })
+    .then(celebrity => {
+      console.log('celeb updated sucessfully', celebrity);
       res.redirect('/celebrities');
+    })
+    .catch(error => {
+      next(error);
     });
 });
 
